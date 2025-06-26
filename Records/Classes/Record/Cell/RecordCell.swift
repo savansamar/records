2a3f9669501b789060d089f5db0fc723627ec657
savansamar <savansoni125@gmail.com>
2025-06-26 16:24:12 +0530

    first commit

---
+++ Records/Classes/Record/Cell/RecordCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordCell.swift
+//  Records
+//
+//  Created by MACM72 on 26/06/25.
+//
+
+import UIKit
+
+class RecordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
